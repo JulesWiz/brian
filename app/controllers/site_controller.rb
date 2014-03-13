6d9cc0cdb718f39129437b3f56121f08b6525eb6
JulesWiz <ngjulie0203@gmail.com>
2014-03-13 10:34:56 +0800

    Added login/logout session.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,7 +2,7 @@ class SiteController < ApplicationController
 
   def index
     # render json: User.all.entries
-    @users = User.all.entries
+    @users = User.all.entries # come from routes.rb #this is the logic
   end
 
   def privacy
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Brian::Application.routes.draw do
 
-  root 'site#index'
+  root 'site#index' #step 1 where browser -> server
+
+  get 'login' => 'session#new'
+  post 'login' => 'session#create'
+  delete 'logout' => 'session#destroy'
+  get 'logout' => 'session#destroy'
 
   get 'privacy' => 'site#privacy'
   get 'terms' => 'site#terms'
 
 end
+
+# the concept of MVC
