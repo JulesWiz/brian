49b727cdabb178cec1c2cd2fb9faf3383da42d7d
JulesWiz <ngjulie0203@gmail.com>
2014-03-17 12:06:44 +0800

    Added SecureRandom.base64 for random password generation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,11 @@ class User
   field :admin, type: Boolean
   field :yearOfKnowing, type: Integer
 
-  before_save :encrypt_password
+  before_save :set_random_password, :encrypt_password
   validates :email, presence: true, uniqueness: {case_sensitive: false}
+  # validates :password, presence: true, uniqueness: {case_sensitive: false}
+  # validates :password_confirmation, presence: true, uniqueness: {case_sensitive: false}
+
 
   def self.authenticate(email, password)
     user = User.find_by email: email
@@ -27,13 +30,20 @@ class User
 
   protected
 
-  def encrypt_password
-    puts "Encrypting the password: #{self.password}"
-    if password.present?
-      self.salt = BCrypt::Engine.generate_salt
-      self.fish = BCrypt::Engine.hash_secret(password, self.salt)
-    true
+    def set_random_password
+      if self.fish.blank? and password.blank?
+        self.salt = BCrypt::Engine.generate_salt
+        self.fish = BCrypt::Engine.hash_secret(SecureRandom.base64(32), self.salt)
+      end
     end
-  end
+
+    def encrypt_password
+      puts "Encrypting the password: #{self.password}"
+      if password.present?
+        self.salt = BCrypt::Engine.generate_salt
+        self.fish = BCrypt::Engine.hash_secret(password, self.salt)
+      true
+      end
+    end
 
 end
