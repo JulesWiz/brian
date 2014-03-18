a225fafdb8acae977da7a5bdfa303054dba419b7
JulesWiz <ngjulie0203@gmail.com>
2014-03-18 17:37:37 +0800

    Added first try on refactoring.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,6 +6,28 @@ class SessionController < ApplicationController
     redirect_to root_url, notice: "You are logged in." if current_user
   end
 
+  def create
+    user = User.find_by(email: params[:user][:email])
+    password = params[:user][:password]
+
+    if user
+      if password.blank?
+        #password reset
+        PasswordResetter.new(flash).handle_password_reset(user)
+        render :new
+      else
+        #authenticate
+        AuthenticateUser.new(session, flash).handle_authenticate_user(user)
+        redirect_to root_url
+      end
+    else
+      # error msg :user not exists
+      flash[:alert]= "email and password wrong !"
+      render :new
+    end
+
+  end
+
   # def create
   #   # render text: "Log #{params[:user][:email]} in with #{params[:user][:password]}."
   #   # @user = User.authenticate(params[:user][:email], params[:user][:password])
