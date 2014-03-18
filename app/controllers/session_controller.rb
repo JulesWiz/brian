8349ec1de5be882ff4e5e5638bb2dceb62f17ffb
JulesWiz <ngjulie0203@gmail.com>
2014-03-18 16:44:45 +0800

    Added

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,30 +6,30 @@ class SessionController < ApplicationController
     redirect_to root_url, notice: "You are logged in." if current_user
   end
 
-  def create
-    # render text: "Log #{params[:user][:email]} in with #{params[:user][:password]}."
-    # @user = User.authenticate(params[:user][:email], params[:user][:password])
-    user = User.find_by(email: params[:user][:email])
-    password = params[:user][:password]
+  # def create
+  #   # render text: "Log #{params[:user][:email]} in with #{params[:user][:password]}."
+  #   # @user = User.authenticate(params[:user][:email], params[:user][:password])
+  #   user = User.find_by(email: params[:user][:email])
+  #   password = params[:user][:password]
 
-    if user and password.blank?
-      # render text: "Time to reset password"
-      user.set_password_reset
-      UserNotifier.reset_password(user).deliver
-      flash.now[:notice] = "We are sending you an email for you to reset your password."
-      render :new
-    elsif user and user.authenticate(password)
-      session[:user_id] = user.id
-      # render text: "Welcome #{@user.email}"
-      redirect_to root_url
-    else
-      # render text: "Who are you?"
-      # redirect_to login_url
-      flash.now[:alert] = "Unable to log you in. Please check your email address and password and sign in again."
-      render :new
-    end
+  #   if user and password.blank?
+  #     # render text: "Time to reset password"
+  #     user.set_password_reset
+  #     UserNotifier.reset_password(user).deliver
+  #     flash.now[:notice] = "We are sending you an email for you to reset your password."
+  #     render :new
+  #   elsif user and user.authenticate(password)
+  #     session[:user_id] = user.id
+  #     # render text: "Welcome #{@user.email}"
+  #     redirect_to root_url
+  #   else
+  #     # render text: "Who are you?"
+  #     # redirect_to login_url
+  #     flash.now[:alert] = "Unable to log you in. Please check your email address and password and sign in again."
+  #     render :new
+  #   end
 
-  end
+  # end
 
   def destroy
     session[:user_id] = nil
