c2bc24c347ac264caaa91ab2d77df441fdb00aef
JulesWiz <ngjulie0203@gmail.com>
2014-03-27 16:21:10 +0800

    Fixed the testing error on email validation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  it "is not valid without email" do
+  it "is valid with an email" do
     user = User.new(
       email: 'ngjulie0203@gmail.com'
     )
   expect(user).to be_valid
   end
 
-  it "is not valid without email" do
+  it "is invalid without email" do
     user = User.new
     expect(user).to be_invalid
   end
 
-  it "is invalid without a salt"
+  it "is invalid without a salt" do
+
 
   it "is invalid without a fish"
 
