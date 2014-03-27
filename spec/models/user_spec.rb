5f2453284dfbd45486d9f7de42e0d839f756a672
JulesWiz <ngjulie0203@gmail.com>
2014-03-27 15:33:12 +0800

    Added testing for email validation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  it  "is not valid without email"
+  it "is not valid without email" do
+    user = User.new(
+      email: 'ngjulie0203@gmail.com'
+    )
+  expect(user).to be_valid
+  end
+
+  it "is not valid without email" do
+    user = User.new
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without a salt"
+
+  it "is invalid without a fish"
+
+
 end
+
